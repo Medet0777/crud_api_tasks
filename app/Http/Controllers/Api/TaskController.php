664dfb3b6fea6ae11bc17d324210a12e01b9555f
Medet0777 <muratbek_05@inbox.ru>
2025-04-17 15:18:04 +0500

    Добавлен TaskController и маршруты для CRUD

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Contracts\TaskServiceContract;
+use App\Http\Requests\TaskCreateRequest;
+use App\Http\Requests\TaskUpdateRequest;
+use Illuminate\Http\JsonResponse;
+
+class TaskController extends Controller
+{
+    protected TaskServiceContract $service;
+
+    public function __construct(TaskServiceContract $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(Request $request): JsonResponse
+    {
+        return $this->service->list();
+    }
+
+    public function show(int $id):JsonResponse
+    {
+        return $this->service->get($id);
+    }
+
+    public function store(TaskCreateRequest $request): JsonResponse
+    {
+        return $this->service->create($request->validated());
+    }
+
+    public function update(TaskUpdateRequest $request, int $id): JsonResponse
+    {
+        return $this->service->update($id, $request->validated());
+    }
+
+    public function destroy(int $id):JsonResponse
+    {
+        return $this->service->delete($id);
+    }
+}
